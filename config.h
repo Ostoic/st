c72461ddc82d6061033ad60ac113c440c4a033e8
Shaun Ostoic <ostoic@uwindsor.ca>
2022-05-03 20:40:31 -0400

    Add st-nord theme patch

--- config.h
+++ config.h
@@ -116,33 +116,33 @@ unsigned int tabspaces = 4;
 float alpha = 0.95;
 float alphaOffset = 0.0;
 float alphaUnfocus;
-
+/* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"#1d1f21",
-	"gray90",
+  /* 8 normal colors */
+  "#3b4252", /* black   */
+  "#bf616a", /* red     */
+  "#a3be8c", /* green   */
+  "#ebcb8b", /* yellow  */
+  "#81a1c1", /* blue    */
+  "#b48ead", /* magenta */
+  "#88c0d0", /* cyan    */
+  "#e5e9f0", /* white   */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 bright colors */
+  "#4c566a", /* black   */
+  "#bf616a", /* red     */
+  "#a3be8c", /* green   */
+  "#ebcb8b", /* yellow  */
+  "#81a1c1", /* blue    */
+  "#b48ead", /* magenta */
+  "#8fbcbb", /* cyan    */
+  "#eceff4", /* white   */
 
-	[255] = 0,
+  [255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+  /* more colors can be added after 255 to use with DefaultXX */
+  "#2e3440", /* background */
+  "#d8dee9", /* foreground */
 };
 
 
@@ -150,11 +150,12 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 6;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
-static unsigned int background = 6;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int background = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
+
 /*
  * Default shape of cursor
  * 2: Block ("█")
